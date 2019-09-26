ac5da9a4bcfa581ed882ade0e8436f9c06d75b0d
Ryan Bell <ryan@piing.net>
2019-09-26 15:54:16 -0700

    React Dev Env (react-scripts)

--- src_frontend_react/src/App.js
+++ src_frontend_react/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
+import Layout from "./layout/Layout";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
+export default () => (
+  <Layout>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Coming Soon!</p>
       </header>
     </div>
-  );
-}
-
-export default App;
+  </Layout>
+);
--- src_frontend_react/src/layout/Layout.js
+++ src_frontend_react/src/layout/Layout.js
@@ -2,6 +2,6 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./theme";
 
-export default ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
+export default ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
