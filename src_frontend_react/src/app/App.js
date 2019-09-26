a6c74465342a0059a04e2afb2f0824cf908d7360
Ryan Bell <ryan@piing.net>
2019-09-26 16:06:40 -0700

    App Directory Restructure

--- src_frontend_react/src/app/App.js
+++ src_frontend_react/src/app/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Landing from "./routes/landing/Landing";
+import Landing from "../routes/landing/Landing";
 
 export default () => (
   <Router>
