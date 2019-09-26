3d949f048f2dbf39b6cfeb0dbe45fcdf466ff4b5
Ryan Bell <ryan@piing.net>
2019-09-26 15:48:00 -0700

    MUI Theme

---
+++ src_frontend_react/layout/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1f55ff"
+    },
+    type: "light",
+    text: {
+      primary: "#101012",
+      secondary: "#545456"
+    }
+  }
+});
