a18e04a676e4b29af836ba0b264516fa0ba590f0
Ryan Bell <ryan@piing.net>
2019-09-26 16:31:58 -0700

    Server status endpoint

--- src_backend_express/app.js
+++ src_backend_express/app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = process.env.PORT || 3001
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3001;
 
-app.use(
-	express.static(
-		path.join(__dirname, '../build_frontend_react')
-	)
-)
+app.use(express.static(path.join(__dirname, "../build_frontend_react")));
 
-app.listen(port, () => console.log(`YQuantify express server listening on port ${port}! \\o/`))
+app.get("/server/health-check", (req, res) => {
+  return res.sendStatus(200);
+});
+
+app.listen(port, () =>
+  console.log(`YQuantify express server listening on port ${port}! \\o/`)
+);
