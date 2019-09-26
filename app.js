ccb77d61b081cac23c59aa6a370b73bda614408d
Ryan Bell <ryan@piing.net>
2019-09-26 12:08:29 -0700

    Automatic deployment flow

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.send("Hello from Render!"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src_backend_express/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(
+	express.static(
+		path.join(__dirname, '../build_frontend_react')
+	)
+)
+
+app.listen(port, () => console.log(`YQuantify express server listening on port ${port}! \\o/`))
