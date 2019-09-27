f980b5dc541b813d1eb5786a3bc13132c0f5b7f9
Ryan Bell <ryan@piing.net>
2019-09-26 19:14:27 -0700

    Fixed path

--- src/backend/express_web_server/src/app.js
+++ src/backend/express_web_server/src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, "../../../public")));
+app.use(express.static(path.join(__dirname, "../../../../public")));
 
 app.get("/server/health-check", (req, res) => {
   return res.sendStatus(200);
