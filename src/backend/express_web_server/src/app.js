61d6a615cb3364c509cdbe9e8d6b5286689ac72c
Ryan Bell <ryan@piing.net>
2019-09-27 20:22:37 -0700

    Stripe payment webhook

--- src/backend/express_web_server/src/app.js
+++ src/backend/express_web_server/src/app.js
@@ -13,6 +13,35 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../../../public/index.html"));
 });
 
+app.post(
+  "/payment",
+  bodyParser.raw({ type: "application/json" }),
+  (request, response) => {
+    let event;
+
+    try {
+      event = JSON.parse(request.body);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case "payment_method.attached":
+        const paymentMethod = event.data.object;
+        handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        return response.status(400).end();
+    }
+
+    response.json({ received: true });
+  }
+);
+
 app.listen(port, () =>
   console.log(`YQuantify express server listening on port ${port}! \\o/`)
 );
