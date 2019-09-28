e330e141cbc024728965828a54119b228618b9ec
Ryan Bell <ryan@piing.net>
2019-09-28 10:54:10 -0700

    Replaced 200 OK with JSON receipt handshake

--- src/backend/express_web_server/src/app.js
+++ src/backend/express_web_server/src/app.js
@@ -29,30 +29,28 @@ app.post(
     switch (event.type) {
       case "invoice.payment_succeeded":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       case "invoice.payment_action_required":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       case "invoice.payment_failed":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       case "customer.subscription.created":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       case "customer.subscription.updated":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       case "customer.subscription.trial_will_end":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       case "customer.subscription.deleted":
         // const paymentData = event.data.object;
-        return response.status(200).end();
+        return response.json({ received: true });
       default:
         return response.status(400).end();
     }
-
-    response.json({ received: true });
   }
 );
 
