87f79f740d5457ba29b955a72a5400aff6d33930
Ryan Bell <ryan@piing.net>
2019-10-02 15:20:56 -0700

    Signup (prototype)

--- src/backend/express_web_server/src/api/post/auth/signup.js
+++ src/backend/express_web_server/src/api/post/auth/signup.js
@@ -24,27 +24,38 @@ module.exports = app => {
     const emailLower = email.toLowerCase();
     const passHash = hash(password);
 
-    pg.connect(process.env.POSTGRE_INTERNAL_CONNECTION, (err, client, done) => {
-      if (err) {
-        return res.json({ error: "db-connection" });
-      }
-      client.query(
-        "INSERT into users (email, password) VALUES($1, $2) RETURNING id",
-        [emailLower, passHash],
-        err => {
-          done();
-
-          if (err) {
-            return res.json({ error: "db-query" });
-          }
-
-          const uid = result.rows[0].number;
-          const payload = { uid, action: "activate" };
-          const token = jwt.sign(payload, process.env.JSON_WEB_TOKEN_SECRET);
-
-          return res.json({ token });
+    pg.connect(
+      process.env.POSTGRE_INTERNAL_CONNECTION,
+      async (err, client, done) => {
+        if (err) {
+          return res.json({ error: "db-connection" });
         }
-      );
-    });
+
+        try {
+          const result = await client.query(query);
+          return res.json({ result });
+        } catch (err) {
+          return res.json({ error: "db-query" });
+        }
+
+        // client.query(
+        //   "INSERT into users (email, password) VALUES($1, $2) RETURNING id",
+        //   [emailLower, passHash],
+        //   err => {
+        //     done();
+
+        //     if (err) {
+        //       return res.json({ error: "db-query" });
+        //     }
+
+        //     const uid = result.rows[0].number;
+        //     const payload = { uid, action: "activate" };
+        //     const token = jwt.sign(payload, process.env.JSON_WEB_TOKEN_SECRET);
+
+        //     return res.json({ token });
+        //   }
+        // );
+      }
+    );
   });
 };
