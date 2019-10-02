ca11dcf8932b4e82bc476e8dc573e4eb371cb1c1
Ryan Bell <ryan@piing.net>
2019-10-02 15:17:23 -0700

    Signup (prototype)

--- src/backend/express_web_server/src/api/post/auth/signup.js
+++ src/backend/express_web_server/src/api/post/auth/signup.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 const validator = require("validator");
 const crypto = require("crypto");
-const { Client } = require("pg");
-const client = new Client();
+const pg = require("pg");
 
 const hash = str =>
   crypto
@@ -25,9 +24,27 @@ module.exports = app => {
     const emailLower = email.toLowerCase();
     const passHash = hash(password);
 
-    const payload = { email: emailLower, password: passHash };
-    const token = jwt.sign(payload, process.env.JSON_WEB_TOKEN_SECRET);
+    pg.connect(process.env.POSTGRE_INTERNAL_CONNECTION, (err, client, done) => {
+      if (err) {
+        return res.json({ error: "db-connection" });
+      }
+      client.query(
+        "INSERT into users (email, password) VALUES($1, $2) RETURNING id",
+        [emailLower, passHash],
+        err => {
+          done();
 
-    return res.json({ token });
+          if (err) {
+            return res.json({ error: "db-query" });
+          }
+
+          const uid = result.rows[0].number;
+          const payload = { uid, action: "activate" };
+          const token = jwt.sign(payload, process.env.JSON_WEB_TOKEN_SECRET);
+
+          return res.json({ token });
+        }
+      );
+    });
   });
 };
