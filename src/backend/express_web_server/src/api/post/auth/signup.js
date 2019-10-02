fdd086d8ffda10a74fe9c2abd126b7c6e21d1d51
Ryan Bell <ryan@piing.net>
2019-10-01 19:26:31 -0700

    Deps: validator

--- src/backend/express_web_server/src/api/post/auth/signup.js
+++ src/backend/express_web_server/src/api/post/auth/signup.js
@@ -1,5 +1,13 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = app => {
   app.post("/auth/signup", (req, res) => {
+    if (!email || !password) {
+      return res.json({ error: "missing-fields" });
+    }
+
+    // process.env.JSON_WEB_TOKEN_SECRET
+
     const { email, password } = req.body;
     return res.json({ email, password });
   });
