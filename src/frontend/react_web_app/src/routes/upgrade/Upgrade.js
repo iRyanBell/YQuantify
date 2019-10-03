c500f208c8a9b3d71c7f8148198c99c694997d3a
Ryan Bell <ryan@piing.net>
2019-10-03 16:20:10 -0700

    Fixed: clientReferenceId scope

--- src/frontend/react_web_app/src/routes/upgrade/Upgrade.js
+++ src/frontend/react_web_app/src/routes/upgrade/Upgrade.js
@@ -82,7 +82,8 @@ export default ({ onDialog }) => {
   const redirectToStripe = async uid => {
     const stripe = window.Stripe("pk_test_Jk5tUWmPGGO41NMhr5T2cgcJ00VtcrxExE");
     const result = await stripe.redirectToCheckout({
-      items: [{ plan: "basic", quantity: 1, clientReferenceId: String(uid) }],
+      items: [{ plan: "basic", quantity: 1 }],
+      clientReferenceId: String(uid),
       successUrl: "https://www.yquantify.com/success",
       cancelUrl: "https://www.yquantify.com/"
     });
