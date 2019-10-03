7057887077e92e562067783aa2a946bbfb8966d9
Ryan Bell <ryan@piing.net>
2019-10-03 16:40:21 -0700

    Added stripe + secret env key

--- src/backend/express_web_server/src/api/post/stripe/webhook.js
+++ src/backend/express_web_server/src/api/post/stripe/webhook.js
@@ -1,3 +1,5 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
 module.exports = (app, pgPool) => {
   app.post("/stripe/webhook", async (req, res) => {
     const sig = req.headers["stripe-signature"];
