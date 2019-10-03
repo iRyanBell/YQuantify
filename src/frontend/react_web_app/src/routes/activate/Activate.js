3205c4cf9f8da2e25115f2979a67cd4e7d76f6b0
Ryan Bell <ryan@piing.net>
2019-10-03 10:49:26 -0700

    Position: static

--- src/frontend/react_web_app/src/routes/activate/Activate.js
+++ src/frontend/react_web_app/src/routes/activate/Activate.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Layout from "../../layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
+import { Paper, Box } from "@material-ui/core";
 
 export default ({ onDialogShow }) => {
   return (
     <Layout>
       <NavBar onDialogShow={onDialogShow} />
+      <Box width="100%" maxWidth={960} marginX="auto">
+        <Paper>
+          <Box marginTop={2} padding={2}>
+            Activate
+          </Box>
+        </Paper>
+      </Box>
     </Layout>
   );
 };
