72b80259619aabe6536778be83947451fecdeb58
Ryan Bell <ryan@piing.net>
2019-10-03 10:29:36 -0700

    Activate + Upgrade routes

--- src/frontend/react_web_app/src/app/App.js
+++ src/frontend/react_web_app/src/app/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Checkout from "../routes/checkout/Checkout";
 import Maintenance from "../routes/maintenance/Maintenance";
 import Landing from "../routes/landing/Landing";
+import Activate from "../routes/activate/Activate";
+import Upgrade from "../routes/upgrade/Upgrade";
 
 export default () => (
   <Router>
     <Route path="/" exact component={Maintenance} />
     <Route path="/checkout" exact component={Checkout} />
     <Route path="/landing" exact component={Landing} />
+    <Route path="/activate" component={Activate} />
+    <Route path="/upgrade" component={Upgrade} />
   </Router>
 );
