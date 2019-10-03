e3531b55ccc511c4c90b2ad24b39435c48204745
Ryan Bell <ryan@piing.net>
2019-10-03 14:44:05 -0700

    invoice.payment_succeeded: UPDATE last_payment_at

--- src/backend/express_web_server/src/api/post/stripe/webhook.js
+++ src/backend/express_web_server/src/api/post/stripe/webhook.js
@@ -1,5 +1,5 @@
 module.exports = (app, pgPool) => {
-  app.post("/payment", (req, res) => {
+  app.post("/payment", async (req, res) => {
     const sig = request.headers["stripe-signature"];
 
     let event;
@@ -21,43 +21,51 @@ module.exports = (app, pgPool) => {
         /* We received a payment! */
         /* Reference: https://stripe.com/docs/billing/lifecycle */
 
-        // const paymentData = event.data.object;
+        const txData = event.data.object;
+        await pgPool.query({
+          text: `
+						UPDATE users
+						SET last_payment_at=CURRENT_TIMESTAMP
+						WHERE id=$1
+					`,
+          values: [txData.uid]
+        });
         return res.json({ received: true });
       case "invoice.payment_action_required":
         /* Regulations in Europe often require 3D Secure. */
         /* Reference: https://stripe.com/docs/billing/subscriptions/payment#handling-action-required */
 
-        // const paymentData = event.data.object;
+        // const txData = event.data.object;
         return res.json({ received: true });
       case "invoice.payment_failed":
         /* Payment failed (bad card). */
         /* Reference: https://stripe.com/docs/billing/migration/invoice-states */
 
-        // const paymentData = event.data.object;
+        // const txData = event.data.object;
         return res.json({ received: true });
       case "customer.subscription.created":
         /* A new customer has signed up, but hasn't paid. */
         /* Reference: https://stripe.com/docs/billing/lifecycle */
 
-        // const paymentData = event.data.object;
+        // const txData = event.data.object;
         return res.json({ received: true });
       case "customer.subscription.updated":
         /* A customer subscription status has changed (failure to pay on time). */
         /* Reference: https://stripe.com/docs/billing/lifecycle */
 
-        // const paymentData = event.data.object;
+        // const txData = event.data.object;
         return res.json({ received: true });
       case "customer.subscription.trial_will_end":
         /* A trial subscription is about to end. */
         /* Reference: https://stripe.com/docs/billing/lifecycle */
 
-        // const paymentData = event.data.object;
+        // const txData = event.data.object;
         return res.json({ received: true });
       case "customer.subscription.deleted":
         /* We lost a customer. */
         /* Reference: https://stripe.com/docs/billing/lifecycle */
 
-        // const paymentData = event.data.object;
+        // const txData = event.data.object;
         return res.json({ received: true });
       default:
         return res.status(400).end();
