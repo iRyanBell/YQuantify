19bd33151fc1e7059ee86086170d54a22238ae54
Ryan Bell <ryan@piing.net>
2019-10-03 15:22:19 -0700

    Fixed: Token

--- src/backend/express_web_server/src/api/post/token/details.js
+++ src/backend/express_web_server/src/api/post/token/details.js
@@ -11,10 +11,7 @@ module.exports = async (app, pgPool) => {
     let uid;
 
     try {
-      const token = jwt.verify(
-        activationToken,
-        process.env.JSON_WEB_TOKEN_SECRET
-      );
+      const tokenDetails = jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET);
       uid = tokenDetails.uid;
     } catch (err) {
       return res.json({ error: "invalid-token" });
