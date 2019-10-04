6588459bee085c02bef2bf2ece485853476adb0c
Ryan Bell <ryan@piing.net>
2019-10-03 18:30:56 -0700

    Updated middleware

--- src/backend/express_web_server/src/attachMiddleware.js
+++ src/backend/express_web_server/src/attachMiddleware.js
@@ -6,20 +6,12 @@ module.exports = app => {
   /* Serve static resources. */
   app.use(static(join(__dirname, "../../../../public")));
 
-  /* Add rawBody (used for Stripe webhook signature verification) */
-  app.use((req, res, next) => {
-    let data_stream = "";
-
-    req
-      .on("data", data => {
-        data_stream += data;
-      })
-      .on("end", () => {
-        req.rawBody = data_stream;
-        next();
-      });
-  });
-
-  /* Use JSON request parsing. */
-  app.use(bodyParser.json());
+  /* Add JSON parsing + rawBody (used for Stripe webhook signature verification) */
+  app.use(
+    bodyParser.json({
+      verify: req => {
+        req.rawBody = buf.toString();
+      }
+    })
+  );
 };
