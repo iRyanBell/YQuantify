569937985d25ba1dc7dd4026d1dee8a64803691f
Ryan Bell <ryan@piing.net>
2019-10-04 14:58:43 -0700

    Moved critical CSS to index.html

--- src/frontend/react_web_app/src/index.js
+++ src/frontend/react_web_app/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./layout/index.css";
 import App from "./app/App";
 import * as serviceWorker from "./app/serviceWorker";
 import "typeface-roboto";
