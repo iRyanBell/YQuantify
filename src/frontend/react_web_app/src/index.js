cf855c0ffb003733f7a059280a679196162a3af8
Ryan Bell <ryan@piing.net>
2019-10-04 15:03:34 -0700

    Non-blocking Roboto WebFont loader

--- src/frontend/react_web_app/src/index.js
+++ src/frontend/react_web_app/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
 import * as serviceWorker from "./app/serviceWorker";
-import "typeface-roboto";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
