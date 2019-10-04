4ed3376214d65a1e4fb9f34f13984d62044399e3
Ryan Bell <ryan@piing.net>
2019-10-04 14:12:31 -0700

    Password reset email

---
+++ src/backend/express_web_server/src/api/post/auth/forgot.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+const crypto = require("crypto");
+const postmark = require("postmark");
+const pmClient = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
+
+module.exports = (app, pgPool) => {
+  app.post("/auth/forgot", async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.json({ error: "missing-fields" });
+    } else if (!validator.isEmail(email)) {
+      return res.json({ error: "malformed-email" });
+    }
+
+    const emailLower = email.toLowerCase();
+
+    try {
+      const { rowCount } = await pgPool.query({
+        text: `
+					SELECT email
+					FROM users
+					WHERE email = $1
+				`,
+        values: [emailLower]
+      });
+      if (rowCount) {
+        return res.json({ error: "email-not-found" });
+      }
+
+      const payload = { uid, action: "reset" };
+      const token = jwt.sign(payload, process.env.JSON_WEB_TOKEN_SECRET);
+
+      pmClient.sendEmailWithTemplate({
+        From: "no-reply@yquantify.com",
+        To: email,
+        TemplateAlias: "forgot",
+        TemplateModel: {
+          reset_url: `https://www.yquantify.com/reset/${token}`,
+          support_email: "support@yquantify.com",
+          product_name: "YQuantify",
+          help_url: "https://www.yquantify.com/docs"
+        }
+      });
+
+      return res.json({ uid });
+    } catch (err) {
+      return res.json({ error: "db-query", "error-details": err });
+    }
+  });
+};
