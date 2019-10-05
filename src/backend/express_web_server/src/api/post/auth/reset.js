28f24f0e9a4e1a91ee3ea85b3d5a4a17954b4cff
Ryan Bell <ryan@piing.net>
2019-10-04 17:05:59 -0700

    Reset endpoint

---
+++ src/backend/express_web_server/src/api/post/auth/reset.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const hash = str =>
+  crypto
+    .createHmac("sha256", process.env.SHA256_SECRET)
+    .update(str)
+    .digest("hex");
+
+module.exports = (app, pgPool) => {
+  app.post("/auth/reset", async (req, res) => {
+    const { password, resetToken } = req.body;
+
+    if (!password || !resetToken) {
+      return res.json({ error: "missing-fields" });
+    }
+
+    const passHash = hash(password);
+    let uid;
+
+    try {
+      const tokenDetails = jwt.verify(
+        activationToken,
+        process.env.JSON_WEB_TOKEN_SECRET
+      );
+      uid = tokenDetails.uid;
+    } catch (err) {
+      return res.json({ error: "invalid-token" });
+    }
+
+    try {
+      await pgPool.query({
+        text: `
+					UPDATE users
+					SET password=$1
+					WHERE id=$2
+				`,
+        values: [passHash, uid]
+      });
+
+      return res.json({ updated: true });
+    } catch (err) {
+      return res.json({ error: "db-query", "error-details": err });
+    }
+  });
+};
