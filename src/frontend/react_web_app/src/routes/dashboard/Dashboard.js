c75b8f52bd973af769d117ce3714560b2c7434d1
Ryan Bell <ryan@piing.net>
2019-10-08 13:00:14 -0700

    Deps: nivo

--- src/frontend/react_web_app/src/routes/dashboard/Dashboard.js
+++ src/frontend/react_web_app/src/routes/dashboard/Dashboard.js
@@ -2,9 +2,12 @@ import React from "react";
 import Layout from "../../layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
-import { Box, Paper, Typography } from "@material-ui/core";
+import { Box, Paper, IconButton, Typography } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import { MdAdd, MdViewList } from "react-icons/md";
 
-const Main = ({ onDialog }) => {
+const Main = () => {
+  const theme = useTheme();
   return (
     <Box
       marginX="auto"
@@ -16,10 +19,28 @@ const Main = ({ onDialog }) => {
     >
       <Box padding={2}>
         <Paper>
-          <Box padding={2}>
+          <Box display="flex" padding={2} alignItems="center">
             <Typography variant="h6" style={{ fontWeight: 800 }}>
               Dashboard
             </Typography>
+            <Box
+              display="flex"
+              flexGrow={1}
+              justifyContent="flex-end"
+              color="#fff"
+            >
+              <IconButton color="inherit">
+                <MdViewList color={theme.palette.primary.main} />
+              </IconButton>
+              <Box marginLeft={1}>
+                <IconButton
+                  color="inherit"
+                  style={{ backgroundColor: theme.palette.primary.main }}
+                >
+                  <MdAdd color="white" />
+                </IconButton>
+              </Box>
+            </Box>
           </Box>
         </Paper>
       </Box>
