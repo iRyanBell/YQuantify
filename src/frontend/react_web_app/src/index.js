83d80e08f5ac6118f81d2e4cee92db06b93053ef
Ryan Bell <ryan@piing.net>
2019-10-08 10:13:25 -0700

    Disabled service worker caching

--- src/frontend/react_web_app/src/index.js
+++ src/frontend/react_web_app/src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from "./app/serviceWorker";
 
 /* Progressive Web App */
 /* REF: https://create-react-app.dev/docs/making-a-progressive-web-app */
-const isPWA = true;
+const isPWA = false; // Disable caching during development.
 
 ReactDOM.render(<App />, document.getElementById("root"));
 isPWA ? serviceWorker.register() : serviceWorker.unregister();
