c91472960545c5c307a150f9d45ef3fb145f793d
Ryan Bell <ryan@piing.net>
2019-10-10 10:04:16 -0700

    Insert query

--- src/backend/express_web_server/src/api/post/entry/add.js
+++ src/backend/express_web_server/src/api/post/entry/add.js
@@ -12,22 +12,33 @@ module.exports = (app, pgPool) => {
           token,
           process.env.JSON_WEB_TOKEN_SECRET
         );
-        return res.json({ tokenDetails });
+        const uid = tokenDetails.uid;
+        const { feature, value } = req.body;
+        const acceptedFeatures = ["weight", "calories", "sleep", "exercise"];
+
+        if (!feature || !value) {
+          return res.json({ error: "missing-fields" });
+        } else if (acceptedFeatures.indexOf(feature) === -1) {
+          return res.json({ error: "feature-not-supported" });
+        }
+
+        const valueFloat = parseFloat(value);
+
+        await pgPool.query({
+          text: `
+						INSERT INTO entries (uid, feature, value)
+						VALUES ($1, $2, $3)
+						RETURNING id
+					`,
+          values: [uid, feature, valueFloat]
+        });
+
+        return res.json({ added: true });
       } catch (err) {
         return res.json({ error: "invalid-token", "error-details": err });
       }
     } else {
       return res.json({ error: "unauthorized" });
     }
-
-    // const { feature, value } = req.body;
-
-    // const acceptedFeatures = ["weight", "calories", "sleep", "exercise"];
-
-    // if (!feature || !value) {
-    //   return res.json({ error: "missing-fields" });
-    // } else if (acceptedFeatures.indexOf(feature) === -1) {
-    //   return res.json({ error: "feature-not-supported" });
-    // }
   });
 };
