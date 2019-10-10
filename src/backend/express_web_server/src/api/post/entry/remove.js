5d0c6d9e21fd4fb48fcda0759fd5a2c820ed7ff3
Ryan Bell <ryan@piing.net>
2019-10-09 20:17:38 -0700

    /entry/remove endpoint auth

---
+++ src/backend/express_web_server/src/api/post/entry/remove.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (app, pgPool) => {
+  app.post("/entry/remove", async (req, res) => {
+    if (req.headers.authorization.startsWith("Bearer ")) {
+      const token = req.headers.authorization.substring(7, req.header.length);
+      try {
+        const tokenDetails = jwt.verify(token);
+        return res.json({ tokenDetails });
+      } catch (err) {
+        return res.json({ token, "error-details": err });
+      }
+    } else {
+      return res.json({ error: "unauthorized" });
+    }
+
+    // const { feature, id } = req.body;
+
+    // const acceptedFeatures = ["weight", "calories", "sleep", "exercise"];
+
+    // if (!feature || !value) {
+    //   return res.json({ error: "missing-fields" });
+    // } else if (acceptedFeatures.indexOf(feature) === -1) {
+    //   return res.json({ error: "feature-not-supported" });
+    // }
+  });
+};
