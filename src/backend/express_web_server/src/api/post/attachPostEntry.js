af980264397cc2634d828219bd69caca05fecf20
Ryan Bell <ryan@piing.net>
2019-10-09 20:13:43 -0700

    /entry/remove endpoint

--- src/backend/express_web_server/src/api/post/attachPostEntry.js
+++ src/backend/express_web_server/src/api/post/attachPostEntry.js
@@ -1,8 +1,14 @@
 const attachEndpointEntryAdd = require("./entry/add");
+const attachEndpointEntryRemove = require("./entry/remove");
 
 module.exports = (app, pgPool) => {
   /* POST /entry/add */
   /* Add a new entry */
   /* Receives feature & value. */
   attachEndpointEntryAdd(app, pgPool);
+
+  /* POST /entry/remove */
+  /* Remove a new entry */
+  /* Receives feature & id. */
+  attachEndpointEntryRemove(app, pgPool);
 };
