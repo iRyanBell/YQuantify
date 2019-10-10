20a106aa6bb1ea5d0b92435dc56befb3598f5030
Ryan Bell <ryan@piing.net>
2019-10-10 10:08:23 -0700

    Prepared delete statement

--- src/backend/express_web_server/src/api/post/entry/remove.js
+++ src/backend/express_web_server/src/api/post/entry/remove.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (app, pgPool) => {
-  app.post("/entry/remove", async (req, res) => {
+  app.post("/entry/add", async (req, res) => {
     if (req.headers.authorization.startsWith("Bearer ")) {
       const token = req.headers.authorization.substring(
         7,
@@ -12,22 +12,48 @@ module.exports = (app, pgPool) => {
           token,
           process.env.JSON_WEB_TOKEN_SECRET
         );
-        return res.json({ tokenDetails });
+        const uid = tokenDetails.uid;
+        const { id } = req.body;
+
+        if (!id) {
+          return res.json({ error: "missing-fields" });
+        }
+
+        const idInt = parseInt(id);
+
+        const { rows } = await pgPool.query({
+          text: `
+						SELECT uid
+						FROM entries
+						WHERE id = $1
+					`,
+          values: [idInt]
+        });
+        if (!rowCount) {
+          return res.json({ error: "id-not-found" });
+        }
+
+        const [row] = rows;
+        const { uid: uidValidate } = row;
+
+        if (uid !== uidValidate) {
+          return res.json({ error: "unauthorized" });
+        }
+
+        await pgPool.query({
+          text: `
+						DELETE FROM entries
+						WHERE id=$1
+					`,
+          values: [id]
+        });
+
+        return res.json({ removed: true });
       } catch (err) {
         return res.json({ error: "invalid-token", "error-details": err });
       }
     } else {
       return res.json({ error: "unauthorized" });
     }
-
-    // const { feature, id } = req.body;
-
-    // const acceptedFeatures = ["weight", "calories", "sleep", "exercise"];
-
-    // if (!feature || !value) {
-    //   return res.json({ error: "missing-fields" });
-    // } else if (acceptedFeatures.indexOf(feature) === -1) {
-    //   return res.json({ error: "feature-not-supported" });
-    // }
   });
 };
