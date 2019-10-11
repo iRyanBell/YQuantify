2409f057f52bc5fa1665d835e182edbfcbe8f406
Ryan Bell <ryan@piing.net>
2019-10-11 15:34:51 -0700

    Try catch blocks

--- src/backend/express_web_server/src/api/get/csv/daily.js
+++ src/backend/express_web_server/src/api/get/csv/daily.js
@@ -4,28 +4,40 @@ const { Parser } = require("json2csv");
 module.exports = (app, pgPool) => {
   app.get("/csv/daily.csv", async (req, res) => {
     const uid = 14;
+    let rows = [];
+    let csv;
 
-    const { rows } = await pgPool.query({
-      text: `
-				SELECT e.created_at::date,
-					AVG(e.value) FILTER (WHERE feature = 'weight') AS weight,
-					AVG(e.value) FILTER (WHERE feature = 'sleep') AS sleep,
-					AVG(e.value) FILTER (WHERE feature = 'calories') AS calories
-				FROM entries e
-				where uid = $1
-				GROUP BY e.created_at::date, uid
-				ORDER BY e.created_at::date
-			`,
-      values: [uid]
-    });
+    try {
+      const result = await pgPool.query({
+        text: `
+					SELECT e.created_at::date,
+						AVG(e.value) FILTER (WHERE feature = 'weight') AS weight,
+						AVG(e.value) FILTER (WHERE feature = 'sleep') AS sleep,
+						AVG(e.value) FILTER (WHERE feature = 'calories') AS calories
+					FROM entries e
+					where uid = $1
+					GROUP BY e.created_at::date, uid
+					ORDER BY e.created_at::date
+				`,
+        values: [uid]
+      });
+      rows = result.rows;
+    } catch (err) {
+      return res.json({ error: "db-query", "error-details": err });
+    }
+
+    try {
+      const fields = ["created_at", "weight", "sleep", "calories"];
+      const opts = { fields };
+      const parser = new Parser(opts);
+
+      csv = parser.parse(rows);
+    } catch (err) {
+      return res.json({ error: "server", "error-details": err });
+    }
 
     res.setHeader("Content-disposition", "attachment; filename=daily.csv");
     res.set("Content-Type", "text/csv");
-
-    const fields = ["created_at", "weight", "sleep", "calories"];
-    const opts = { fields };
-    const parser = new Parser(opts);
-    const csv = parser.parse(rows);
 
     return res.status(200).send(csv);
     // if (req.headers.authorization.startsWith("Bearer ")) {
