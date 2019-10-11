a723bf9cd0a6c3c7ba3571827efdf917da95388e
Ryan Bell <ryan@piing.net>
2019-10-11 16:58:44 -0700

    Fixed: pgPool attachment

--- src/backend/express_web_server/src/api/get/attachGetCsv.js
+++ src/backend/express_web_server/src/api/get/attachGetCsv.js
@@ -1,8 +1,8 @@
 const attachEndpointCsvDaily = require("./csv/daily");
 
-module.exports = app => {
+module.exports = (app, pgPool) => {
   /* GET /csv/daily */
   /* Receive start and end date /*
 	/* Returns daily dataframe in .csv format. */
-  attachEndpointCsvDaily(app);
+  attachEndpointCsvDaily(app, pgPool);
 };
--- src/backend/express_web_server/src/api/get/csv/daily.js
+++ src/backend/express_web_server/src/api/get/csv/daily.js
@@ -1,12 +1,43 @@
+const { Parser } = require("json2csv");
+
 module.exports = (app, pgPool) => {
   app.get("/csv/daily.csv", async (req, res) => {
-    const result = await pgPool.query({
-      text: `
-				SELECT * FROM entries WHERE uid=$1
-			`,
-      values: [14]
-    });
+    const uid = 14;
+    let rows = [];
+    let csv;
 
-    return res.json({ debug: result });
+    try {
+      const result = await pgPool.query({
+        text: `
+					SELECT e.created_at::date,
+						AVG(e.value) FILTER (WHERE feature='weight') AS weight,
+						AVG(e.value) FILTER (WHERE feature='sleep') AS sleep,
+						AVG(e.value) FILTER (WHERE feature='calories') AS calories
+					FROM entries e
+					WHERE uid=$1
+					GROUP BY e.created_at::date, uid
+					ORDER BY e.created_at::date
+				`,
+        values: [uid]
+      });
+      rows = result.rows;
+    } catch (err) {
+      return res.json({ error: "db-query", "error-details": err });
+    }
+
+    try {
+      const fields = ["created_at", "weight", "sleep", "calories"];
+      const opts = { fields };
+      const parser = new Parser(opts);
+
+      csv = parser.parse(rows);
+    } catch (err) {
+      return res.json({ error: "server", "error-details": err });
+    }
+
+    res.setHeader("Content-disposition", "attachment; filename=daily.csv");
+    res.set("Content-Type", "text/csv");
+
+    return res.send(csv);
   });
 };
