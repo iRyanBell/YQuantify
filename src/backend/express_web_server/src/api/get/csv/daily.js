cdaaa44f7dd0eea9a2e3579b576655e9ab993157
Ryan Bell <ryan@piing.net>
2019-10-11 16:53:31 -0700

    Debug: pg

--- src/backend/express_web_server/src/api/get/csv/daily.js
+++ src/backend/express_web_server/src/api/get/csv/daily.js
@@ -1,59 +1,12 @@
-const jwt = require("jsonwebtoken");
-const { Parser } = require("json2csv");
-
 module.exports = (app, pgPool) => {
   app.get("/csv/daily.csv", async (req, res) => {
-    const uid = 14;
-    let rows = [];
-    let csv;
+    const result = await pgPool.query({
+      text: `
+				SELECT * FROM entries WHERE uid=$1
+			`,
+      values: [14]
+    });
 
-    try {
-      const result = await pgPool.query({
-        text: `
-					SELECT * FROM entries WHERE uid=$1
-				`,
-        values: [uid]
-      });
-      return res.json({ debug: result });
-      // rows = result.rows;
-    } catch (err) {
-      return res.json({ error: "db-query", "error-details": err });
-    }
-
-    try {
-      const fields = ["created_at", "weight", "sleep", "calories"];
-      const opts = { fields };
-      const parser = new Parser(opts);
-
-      csv = parser.parse(rows);
-    } catch (err) {
-      return res.json({ error: "server", "error-details": err });
-    }
-
-    res.setHeader("Content-disposition", "attachment; filename=daily.csv");
-    res.set("Content-Type", "text/csv");
-
-    return res.send(csv);
-    // if (req.headers.authorization.startsWith("Bearer ")) {
-    //   const token = req.headers.authorization.substring(
-    //     7,
-    //     req.headers.authorization.length
-    //   );
-    //   try {
-    //     const tokenDetails = jwt.verify(
-    //       token,
-    //       process.env.JSON_WEB_TOKEN_SECRET
-    //     );
-
-    //     return res.json({ success: true });
-    //   } catch (err) {
-    //     return res.json({
-    //       error: "invalid-token",
-    //       "error-details": err
-    //     });
-    //   }
-    // } else {
-    //   return res.json({ error: "unauthorized" });
-    // }
+    return res.json({ debug: result });
   });
 };
