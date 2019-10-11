3109428455bc0fa937209a540e9cdae1bb389619
Ryan Bell <ryan@piing.net>
2019-10-11 14:16:53 -0700

    Disabled token check during dev

--- src/backend/express_web_server/src/api/get/csv/daily.js
+++ src/backend/express_web_server/src/api/get/csv/daily.js
@@ -2,26 +2,27 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (app, pgPool) => {
   app.post("/csv/daily", async (req, res) => {
-    if (req.headers.authorization.startsWith("Bearer ")) {
-      const token = req.headers.authorization.substring(
-        7,
-        req.headers.authorization.length
-      );
-      try {
-        const tokenDetails = jwt.verify(
-          token,
-          process.env.JSON_WEB_TOKEN_SECRET
-        );
+    return res.json({ success: true });
+    // if (req.headers.authorization.startsWith("Bearer ")) {
+    //   const token = req.headers.authorization.substring(
+    //     7,
+    //     req.headers.authorization.length
+    //   );
+    //   try {
+    //     const tokenDetails = jwt.verify(
+    //       token,
+    //       process.env.JSON_WEB_TOKEN_SECRET
+    //     );
 
-        return res.json({ success: true });
-      } catch (err) {
-        return res.json({
-          error: "invalid-token",
-          "error-details": err
-        });
-      }
-    } else {
-      return res.json({ error: "unauthorized" });
-    }
+    //     return res.json({ success: true });
+    //   } catch (err) {
+    //     return res.json({
+    //       error: "invalid-token",
+    //       "error-details": err
+    //     });
+    //   }
+    // } else {
+    //   return res.json({ error: "unauthorized" });
+    // }
   });
 };
