55e966613b84176d8f55ec2b86d912d470d8a2de
Ryan Bell <ryan@piing.net>
2019-10-12 12:05:25 -0700

    Refactor

--- src/backend/express_web_server/src/api/post/analysis/get.js
+++ src/backend/express_web_server/src/api/post/analysis/get.js
@@ -6,10 +6,10 @@ module.exports = (app, pgPool) => {
       const token = req.headers.authorization.substring(
         7,
         req.headers.authorization.length
-			);
-			let uid
+      );
+      let uid;
 
-			try {
+      try {
         const tokenDetails = jwt.verify(
           token,
           process.env.JSON_WEB_TOKEN_SECRET
@@ -19,37 +19,39 @@ module.exports = (app, pgPool) => {
         return res.json({ error: "invalid-token" });
       }
 
-			const { analysis } = req.body;
-			const acceptedAnalysis = ["weight_sensitivity"];
+      const { analysis } = req.body;
+      const acceptedAnalysis = ["weight_sensitivity"];
 
-			if (!analysis) {
-				return res.json({ error: "missing-fields" });
-			} else if (acceptedAnalysis.indexOf(feature) === -1) {
-				return res.json({ error: "analysis-not-supported" });
-			}
+      if (!analysis) {
+        return res.json({ error: "missing-fields" });
+      } else if (acceptedAnalysis.indexOf(feature) === -1) {
+        return res.json({ error: "analysis-not-supported" });
+      }
 
-			let resultsVal
+      let resultsVal;
 
-			try {
-				const { rows } = await pgPool.query({
-					text: `
+      try {
+        const { rows } = await pgPool.query({
+          text: `
 						SELECT value
 						FROM cache_analysis
 						WHERE id=$1
 					`,
-					values: [`${uid}:${analysis}`]
-				});
-				const [row] = rows;
-				resultsVal = row.value
-			} catch (err) {
-				return res.json({ error: "server", 'error-details': err });
-			}
+          values: [`${uid}:${analysis}`]
+        });
+        const [row] = rows;
+        resultsVal = row.value;
+      } catch (err) {
+        return res.json({ error: "server", "error-details": err });
+      }
 
-			try {
-				const results = JSON.parse(resultsVal);
-				return res.json({ results });
-			} catch (err) {
-				return res.json({ error: "server", 'error-details': err });
-			}
+      try {
+        const results = JSON.parse(resultsVal);
+        return res.json({ results });
+      } catch (err) {
+        return res.json({ error: "server", "error-details": err });
+      }
+    }
+    return res.json({ error: "unauthorized" });
   });
 };
