08c0781efa6239bda9c05d1d20883a4837466274
Ryan Bell <ryan@piing.net>
2019-10-12 12:13:47 -0700

    Patched results to getAnalysis

--- src/frontend/react_web_app/src/routes/dashboard/Dashboard.js
+++ src/frontend/react_web_app/src/routes/dashboard/Dashboard.js
@@ -260,7 +260,7 @@ const Main = ({ onDialog }) => {
 
       axios
         .post("/analysis/get", { analysis }, config)
-        .then(console.log)
+        .then(({ data }) => resolve(data.results))
         .catch(reject);
     });
 
