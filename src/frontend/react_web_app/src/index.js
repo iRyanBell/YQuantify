8e17f126946966abdf61c0ca0f6a776eabc37266
Ryan Bell <ryan@piing.net>
2019-10-12 19:33:14 -0700

    Deps: react-ga

--- src/frontend/react_web_app/src/index.js
+++ src/frontend/react_web_app/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
 import * as serviceWorker from "./app/serviceWorker";
+import ReactGA from "react-ga";
+
+const initializeReactGA = () => {
+  ReactGA.initialize("UA-63336980-9");
+  ReactGA.pageview("/");
+};
 
 /* Progressive Web App */
 /* REF: https://create-react-app.dev/docs/making-a-progressive-web-app */
@@ -9,3 +15,5 @@ const isPWA = false; // Disable caching during development.
 
 ReactDOM.render(<App />, document.getElementById("root"));
 isPWA ? serviceWorker.register() : serviceWorker.unregister();
+
+initializeReactGA();
