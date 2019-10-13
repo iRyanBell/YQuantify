0118be253211e62278e4d0aa419756aa17146fde
Ryan Bell <ryan@piing.net>
2019-10-12 19:50:33 -0700

    Attached history to ReactGA

--- src/frontend/react_web_app/src/app/App.js
+++ src/frontend/react_web_app/src/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 // import Maintenance from "../routes/maintenance/Maintenance";
 import Landing from "../routes/landing/Landing";
 import Dashboard from "../routes/dashboard/Dashboard";
@@ -10,21 +10,24 @@ import Docs from "../routes/docs/Docs";
 import Reset from "../routes/reset/Reset";
 import jwt from "jsonwebtoken";
 import ReactGA from "react-ga";
+import { createBrowserHistory as createHistory } from "history";
 
 export default () => {
   const token = window.localStorage.getItem("token");
   const auth = (token && jwt.decode(token)) || {};
   const isSignedIn = Boolean(auth.uid);
 
-  const initializeReactGA = () => {
-    ReactGA.initialize("UA-63336980-9");
-    ReactGA.pageview("/");
-  };
+  ReactGA.initialize("UA-63336980-9");
 
-  initializeReactGA();
+  const history = createHistory();
+  history.listen(({ pathname: page }) => {
+    console.log(page);
+    ReactGA.set({ page });
+    ReactGA.pageview(page);
+  });
 
   return (
-    <Router>
+    <Router history={history}>
       {/* <Route path="/dashboard" exact component={Dashboard} /> */}
       {/* <Route path="/landing" exact component={Landing} /> */}
       <Route path="/" exact component={isSignedIn ? Dashboard : Landing} />
